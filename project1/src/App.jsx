6e9451c6a6f6753c8820683d4345366f4e06319e
Pritam Roy <roypritam3114@gmail.com>
2025-08-03 13:25:25 +0530

    structure

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -3,9 +3,19 @@ const App = () => {
     <div>
       <nav>
         <div className="logo">
-          <img src="/images/brand_logo.png" alt="" />
+          <img src="/images/brand_logo.png" alt="logo" />
+
 
         </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        
+        <button>login</button>
+
       </nav>
       
     </div>
