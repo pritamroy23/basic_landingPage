297d4285e1de8cee85b27f73306e7f8d4c1950ba
Pritam Roy <roypritam3114@gmail.com>
2025-08-06 13:31:32 +0530

    component created

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,24 +1,9 @@
 import "./APP.css"
+import Navigation from "./Component/Navigation";
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo" />
-
-
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        
-        <button>login</button>
-
-      </nav>
-      
+      <Navigation />
     </div>
   );
 };
