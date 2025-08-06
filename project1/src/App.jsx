f39084c7378ec60a5136629da691bdbe0e9fcae0
Pritam Roy <roypritam3114@gmail.com>
2025-08-06 15:14:21 +0530

    .

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,10 +1,11 @@
 import "./APP.css"
 import Navigation from "./Component/Navigation";
+import Hero from "./Component/hero";
 const App = () => {
   return (
     <div>
       <Navigation />
-      <hero/>
+      <Hero/>
     </div>
   );
 };
