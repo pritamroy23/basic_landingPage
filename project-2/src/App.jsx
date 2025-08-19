d066ff650a16714d41d03c99fccdcf98c429e15b
Pritam Roy <roypritam3114@gmail.com>
2025-08-19 16:37:01 +0530

    .

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,12 +1,17 @@
 
-import './App.css'
+import './App.css';
+import Navigation from './components/navigation';
 
 function App() {
-  
+  // This is the main App component that renders the Navigation component
+  // and can be extended with more components in the future.
+  // It serves as the entry point for the application.
 
   return (
-    <div></div>
-  )
+    <div>
+      <Navigation />
+    </div>
+  );
 }
 
 export default App
---
+++ project-2/src/components/navigation.jsx
@@ -0,0 +1,20 @@
+
+
+const navigation = () => {
+  return (
+    <nav>
+        <div className="logo">
+            <img src="/images/logo.png" alt="" />
+        </div>
+
+        <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>contact</li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default navigation;
